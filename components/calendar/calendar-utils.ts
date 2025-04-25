9bc2fca13925332b16e33177d5415642e7d19042
mohammad <mohammadkarimi.swctw@gmail.com>
2025-04-25 20:38:16 +0330

    feat: add calendar utility functions

---
+++ components/calendar/calendar-utils.ts
@@ -0,0 +1,18 @@
+import {
+  addYears as addYearsJalali,
+  startOfYear as startOfYearJalali,
+  subYears as subYearsJalali,
+} from "date-fns-jalali";
+import { addYears, startOfYear, subYears } from "date-fns-jalali";
+
+export const START_YEAR_JALALI = subYearsJalali(
+  startOfYearJalali(new Date()),
+  12
+);
+export const END_YEAR_JALALI = addYearsJalali(
+  startOfYearJalali(new Date()),
+  12
+);
+
+export const START_YEAR_GREGORIAN = subYears(startOfYear(new Date()), 12);
+export const END_YEAR_GREGORIAN = addYears(startOfYear(new Date()), 12);
