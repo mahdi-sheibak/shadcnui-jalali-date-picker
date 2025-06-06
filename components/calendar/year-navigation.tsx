b371b286faffc71d51b20c14023ac9c4ce50ae38
mohammad <mohammadkarimi.swctw@gmail.com>
2025-06-06 14:35:00 +0330

    chore: format year-navigation

--- components/calendar/year-navigation.tsx
+++ components/calendar/year-navigation.tsx
@@ -1,15 +1,15 @@
-import type { NextMonthButtonProps } from "react-day-picker";
+import type { NextMonthButtonProps } from 'react-day-picker';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-import { Button } from "../ui/button";
+import { Button } from '../ui/button';
 
 interface YearNavigationProps {
   monthContent: React.JSX.Element;
   monthClassName?: string;
   yearContent: React.JSX.Element;
   yearClassName?: string;
-  yearDirection: "next" | "prev";
+  yearDirection: 'next' | 'prev';
   isDisabled: boolean;
   onClick: () => void;
   props: NextMonthButtonProps;
@@ -25,22 +25,16 @@ export default function YearNavigation({
   onClick,
   props,
 }: YearNavigationProps) {
-  const isNextYear = yearDirection === "next";
+  const isNextYear = yearDirection === 'next';
 
   return (
     <>
-      <Button
-        {...props}
-        className={cn(monthClassName, props.className)}
-        variant="outline"
-      >
+      <Button {...props} className={cn(monthClassName, props.className)} variant="outline">
         {monthContent}
       </Button>
       <Button
         size="icon"
-        aria-label={
-          isNextYear ? "Go to the Next Year" : "Go to the Previous Year"
-        }
+        aria-label={isNextYear ? 'Go to the Next Year' : 'Go to the Previous Year'}
         className={cn(yearClassName, props.className)}
         disabled={isDisabled}
         variant="outline"
